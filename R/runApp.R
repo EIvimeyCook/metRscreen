c613392fdc4b4878290b7b84ed54bf10ade84b80
Ed Ivimey-Cook <e.ivimeycook@gmail.com>
2024-04-15 13:37:11 +0100

    updated with new manual

--- R/runApp.R
+++ R/runApp.R
@@ -5,7 +5,7 @@
 #' @param reject.vec vector of rejection reasons to be added to metRscreen, can be left empty
 #' @export
 
-metRscreen <- function(screen.file, reject.vec = NULL, ) {
+metRscreen <- function(screen.file, reject.vec = NULL) {
   # if data.str if missing, assign an empty data.frame
   if (missing(screen.file)) cat("\nError: Please provide a .csv file to screen\n")
   if (missing(reject.vec)) reject.vec <- NULL
